395b822bb57863c4d90c94bfa7f0f3f73101a933
Nguyen Kim Thach <nguyenkimthach.a@gmail.com>
2023-12-14 11:27:37 +0700

    Init maven cucumber framework

---
+++ src/test/java/stepDefinitions/FacebookSteps.java
@@ -0,0 +1,39 @@
+package stepDefinitions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FacebookSteps {
+	WebDriver driver;
+
+	@Given("Open facebook application")
+	public void openFacebookApplication() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Then("Verify email textbox is displayed")
+	public void verifyEmailTextboxIsDisplayed() {
+		Assert.assertTrue(driver.findElement(By.id("email")).isDisplayed());
+	}
+
+	@Then("Verify password textbox is displayed")
+	public void verifyPasswordTextboxIsDisplayed() {
+		Assert.assertTrue(driver.findElement(By.id("pass")).isDisplayed());
+	}
+
+	@Then("Close applcation")
+	public void closeApplcation() {
+		driver.quit();
+	}
+}
